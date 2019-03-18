f7f1c40fcdbeee659010f39915f3763df1433fd0
Core Ireland <irelandco@icloud.com>
2019-03-18 16:43:33 -0400

    Prototype Dubin's Curve stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spookyPathing.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/spookyPathing.java
@@ -1,11 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import org.firstinspires.ftc.teamcode.Subsystems.Node;
+import org.firstinspires.ftc.teamcode.Subsystems.myArc;
+
 public class spookyPathing {
 
-    static double x1, x2, y1, y2, ang1, ang2;
-    static double arc1;
+    static Node start = new Node(0, 0, 0);
+    static Node end = new Node(4,0, 0);
+
+    static myArc firstArc = new myArc(start, true);
+    static myArc secondArc = new myArc(end, true);
 
     public static void main(String args[]) {
+        double xDiff = secondArc.findCenter().x - firstArc.findCenter().x;
+        double yDiff = secondArc.findCenter().y - firstArc.findCenter().y;
+        double slope = Math.atan(yDiff / xDiff);
 
+        firstArc.setLength((-slope + (Math.PI/2)) - start.ang);
+        secondArc.setLength(end.ang - (-slope + (Math.PI/2)));
+
+        System.out.println("Slope: " + (-Math.toDegrees(slope) + 90));
+        System.out.println("First Arc: " + Math.toDegrees(firstArc.length));
+        System.out.println("Second Arc: " + Math.toDegrees(secondArc.length));
     }
 }
